f7e8bdd04496485b406a9ad476485d76e51bdcdf
Vincent Balsan <vincent.balsan@jci.com>
2022-09-10 19:10:14 +0100

    IR remote operational

--- main.c
+++ main.c
@@ -11,7 +11,10 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "ws2811.pio.h"
+
+#include "generated/ws2811.pio.h"
+
+#include "nec_receive_library/nec_receive.h"
 
 #define IS_RGBW false
 #define NUM_PIXELS 50
@@ -40,27 +43,50 @@ int main() {
 
     stdio_init_all();
 
-    // todo get free sm
-    PIO pio = pio0;
+    // for ws2811
+    PIO pio2811 = pio0;
     int sm = 0;
-    uint offset = pio_add_program(pio, &ws2811_program);
+    uint offset = pio_add_program(pio2811, &ws2811_program);
+
+    // for ir
+    PIO pioIR = pio1;                                 
+    uint rx_gpio = 15;                              
+    int rx_sm = nec_rx_init(pioIR, rx_gpio);         // uses one state machine and 9 instructions
 
 
-    ws2811_program_init(pio, sm, offset, WS2811_PIN, 800000);
+    if (offset == -1 || rx_sm == -1) {
+        printf("could not configure PIO\n");
+        return -1;
+    }
 
-    uint8_t g = 25;
-    uint8_t r = 255;    
-    uint8_t b = 0;
+
+    ws2811_program_init(pio2811, sm, offset, WS2811_PIN, 800000);
 
     uint8_t gidx = 0;
     uint8_t ridx = 0;    
     uint8_t bidx = 0;
 
+    uint8_t main_val   = 0;
+    uint8_t main_val10 = 0;
+    uint8_t main_val50 = 0;
+
+    uint8_t *g, *r, *b;
+    g = &main_val10;
+    r = &main_val;    
+    b = &main_val50;
+
     uint32_t pixel;
+
+    // transmit and receive frames
+    uint8_t rx_address, rx_data;
 
     while (1) 
     {
-        pixel = ugrb_u32( g,  r,  b);
+        main_val = red_arr_breathing_2[++ridx]; 
+        main_val10 = main_val / 10u;
+        main_val50 = main_val / 50u;
+
+        pixel = ugrb_u32( *g,  *r,  *b);
 
         for (int i = 0; i < NUM_PIXELS -1; ++i) 
         {            
@@ -69,13 +95,71 @@ int main() {
         }
         put_pixel(pixel);
 
-        sleep_ms(100);
+        sleep_ms(35);
         
+        //r = 255;
+        //g = 25;
+        //b =0;
         //g = green_arr[++gidx];
-        r = red_arr[++ridx];
-        g = (uint8_t)(r/10u);
-        //b = blu_arr[++bidx];
-        b = (uint8_t)(r/50u);
+        //r = red_arr[++ridx];
+        
+        // r = red_arr_breathing_2[++ridx];
+        // g = (uint8_t)(r/10u);
+        // //b = blu_arr[++bidx];
+        // b = (uint8_t)(r/50u);
+
+        /*
+        printf("\t\n");
+        printf("\t\n idx : %d", ridx);
+        printf("\t\n value : %d", r);
+        printf("\t\n size : %d", sizeof(red_arr_breathing));
+        */
+
+        
+        // Check if remote has sent message
+        while (!pio_sm_is_rx_fifo_empty(pioIR, rx_sm)) 
+        {
+            uint32_t rx_frame = pio_sm_get(pioIR, rx_sm);
+
+            if (nec_decode_frame(rx_frame, &rx_address, &rx_data)) 
+            {
+                printf("\t\nreceived: %02x, %02x", rx_address, rx_data);
+                if(rx_address == 0x00)
+                {
+                    switch (rx_data)
+                    {
+                    case 0x45:
+                        g = &main_val10;
+                        r = &main_val;    
+                        b = &main_val50;
+                        break;
+
+                    case 0x44:
+                        r = &main_val10;
+                        g = &main_val;    
+                        b = &main_val50;
+                        break;
+
+                    case 0x07:
+                        g = &main_val10;
+                        b = &main_val;    
+                        r = &main_val50;
+                        break;
+                    
+                    default:
+                        g = &main_val10;
+                        r = &main_val;    
+                        b = &main_val50;
+                        break;
+                    }
+                }
+            } 
+            else 
+            {
+                printf("\treceived: %08x", rx_frame);
+            }
+        }
+
 
         /*
         g = 0;
